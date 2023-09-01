96f24b18ad277d3979e450ab822f17dfc258ae22
survani <giovaniportal@gmail.com>
2023-08-31 20:36:50 -0700

    Brought Back Clerk
    Need to figure out why production is stale and creates a 404

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/navigation/navbar";
 import "./globals.css";
 import RightSidebar from "./components/sidebars/rightSideBar";
-// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
   title: "MythsNoMore - Debunking Myths",
@@ -10,16 +10,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="font-sans flex bg-[#1f2937]">
-        <Navbar />
-        <div className="flex-grow">
-          <div className="flex flex-col h-screen appBg">
-            <div className="flex-grow">{children}</div>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="font-sans flex bg-[#1f2937]">
+          <Navbar />
+          <div className="flex-grow">
+            <div className="flex flex-col h-screen appBg">
+              <div className="flex-grow">{children}</div>
+            </div>
           </div>
-        </div>
-        <RightSidebar />
-      </body>
-    </html>
+          <RightSidebar />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
